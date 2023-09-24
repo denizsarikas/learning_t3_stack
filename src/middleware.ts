f37318eba809fc8cd91d61a5589ffd487e7f57ea
Deniz Sarıkaş <denizsarikas@hotmail.com>
2023-09-24 16:01:03 +0300

    add auth

---
+++ src/middleware.ts
@@ -0,0 +1,18 @@
+
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+import { authMiddleware } from "@clerk/nextjs";
+export default authMiddleware({
+  // "/" will be accessible to all users
+  publicRoutes: ["/"]
+});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+
+// export default () => {
+//     "ok"
+// }
