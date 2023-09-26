6536f7a1ae4f4399d822e7c3e33faa9fedb22832
Deniz Sarıkaş <denizsarikas@hotmail.com>
2023-09-26 22:28:29 +0300

    37.47

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,10 +1,44 @@
+import { clerkClient } from "@clerk/nextjs";
+import { User } from "@clerk/nextjs/dist/types/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const postRouter = createTRPCRouter({
+const filterUserForClient = (user: User) => {
+  return {
+    id: user.id,
+    username: user.username,
+    profilePicture: user.profileImageUrl,
+  };
+};
 
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+export const postRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+    const authorIds = posts.map((post) => post.authorId);
+    const users = await clerkClient.users.getUserList({
+      userId: authorIds,
+      limit: 100,
+    });
+    const filteredUsers = users.map(filterUserForClient);
+    const postsWithAuthors = posts.map((post) => {
+      const author = filteredUsers.find((users) => users.id === post.authorId);
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Author for post not found",
+        });
+      }
+      return {
+        post,
+        author,
+      };
+    });
+    return postsWithAuthors;
   }),
 });
+
+
